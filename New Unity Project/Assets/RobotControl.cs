8f208ea88a9d9f4cddbab62e2e02aad624fb85d4
fhollander <forestholander@gmail.com>
2020-04-07 18:23:15 -0400

    commit

---
+++ New Unity Project/Assets/RobotControl.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RobotControl : MonoBehaviour
+
+{
+
+	public GameObject selectedGear;
+	public List<GameObject> gears;
+	
+	
+	
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
